55481dc448eb1f55225c0350d5ef132b677bacd6
DuongBaNhat <banhatduong@gmail.com>
2022-10-19 17:23:12 +0700

    add aiprespone in controller & lib swagger

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,23 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { User } from '../entitiy/user.entity';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) { }
 
     @Get()
+
     getAll() {
         return this.userService.getAll();
     }
 
     @Post()
+    @ApiResponse({
+        status: 300,
+        description: 'hhhh'
+    })
     create(@Body() user: User) {
         return this.userService.create(user);
     }
